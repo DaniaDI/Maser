0321b7b06c9515bed02b9d3fcbad5a7285e3bc34
Dania Danial <daniaisead2002@gmail.com>
2026-09-15 23:50:05 +0300

    TASK2 JS edit

--- Maser/js/task2.js
+++ Maser/js/task2.js
@@ -2,6 +2,34 @@ let passedStudents = 0;
 let failedStudents = 0;
 let total = 0;
 
+
+//WITHOUT FUNCTION
+let grades = [30, 50, 60, 90];
+
+for (let i = 0; i < grades.length; i++) {
+    if (grades[i] >= 60) {
+        console.log("Student passed");
+        passedStudents++;
+    } else {
+        console.log("Student failed");
+        failedStudents++;
+    }
+    total += grades[i];
+}
+
+let avg = total / grades.length;
+console.log("Total number of passed students: " + passedStudents);
+console.log("Total number of failed students: " + failedStudents);
+console.log("Total grades: " + total);
+console.log("Average grade: " + avg);
+console.log("**-----------------------------------**");
+
+//USING FUNCTION 
+
+/*here reset the global variables*/
+passedStudents = 0; 
+failedStudents = 0; 
+total = 0;
 function grade(arr) {
 
     for (let i = 0; i < arr.length; i++) {
@@ -26,4 +54,3 @@ function grade(arr) {
 }
 
 grade([30, 50, 60, 90]);
-
